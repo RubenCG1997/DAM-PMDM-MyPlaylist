7a3eb8ef604421253fe759e446d3b67e120064c3
RubenCG1997 <camachogomez1597@gmail.com>
2024-12-02 21:06:06 +0100

    Se añade icono a la app y se corrige un par de fallitos

--- app/src/main/java/com/iescamas/myplaylist/ControladorMusica.java
+++ app/src/main/java/com/iescamas/myplaylist/ControladorMusica.java
@@ -12,10 +12,9 @@ public class ControladorMusica {
     }
 
     public static void initMusica(){
-        Cancion cancion;
         listaCancion.add(new Cancion("ESTOY PUTIAO","Feid",R.drawable.imgdisco1,R.raw.estoy_putiao));
         listaCancion.add(new Cancion("PPCP","Feid",R.drawable.imgdisco1,R.raw.pcpp));
-        listaCancion.add(new Cancion("1XXXX RAXONES","Feid",R.drawable.imgdisco1,R.raw.unoxxx_razones));
+        listaCancion.add(new Cancion("1XXX RAXONES","Feid",R.drawable.imgdisco1,R.raw.unoxxx_razones));
         listaCancion.add(new Cancion("MONI","Feid",R.drawable.imgdisco1,R.raw.moni));
         listaCancion.add(new Cancion("DE REPENTE","Feid",R.drawable.imgdisco1,R.raw.de_repente));
         listaCancion.add(new Cancion("AGAIN","Feid",R.drawable.imgdisco1,R.raw.again));
